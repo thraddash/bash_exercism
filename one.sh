e81abf47f3bc88bfdcbd329262f348a3370846e3
thraddash <theartist@gmail.com>
2021-11-18 18:31:21 -0800

    initial commit

---
+++ one.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+var=$1
+name=${var:=you}
+echo "name=\${var:=you}"
+echo "If var is null or unset, var is set to the value of you."
+echo ""
+echo "One for $name, one for me."
+echo "\$var: $var"
+echo "\$name: $name"
+echo "###############################"
+echo ""
+var=$1
+name=${var:-you}
+echo "name=\${var:-you}"
+echo "If var is null or unset, you is substituted for var."
+echo "The value of var does not change."
+echo ""
+echo "One for $name, one for me."
+echo "\$var: $var"
+echo "\$name: $name"
+echo "###############################"
+echo ""
+echo "One for ${1:-you}, one for me."
+echo "###############################"
+echo ""
+name=$1
+if [ -z "$name" ]; then
+  echo "One for you, one for me."
+else
+  echo "One for $name, one for me."
+fi
