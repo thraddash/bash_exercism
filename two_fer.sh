b24c5ef64b31289ac57c87018ffdf5165be55328
thraddash <theartist@gmail.com>
2021-11-18 18:39:19 -0800

    add more comments

--- two_fer.sh
+++ two_fer.sh
@@ -30,3 +30,9 @@ if [ -z "$name" ]; then
 else
   echo "One for $name, one for me."
 fi
+
+## echo "${var:?message}"
+## echo "If var is null or unset, message is printed to standard error. This checks that variables are set correctly."
+    
+## echo "${var:+word}"
+## echo "If var is set, word is substituted for var. The value of var does not change."
